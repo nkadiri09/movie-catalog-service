d671fed1d16a625f0cc6f6605d9f03b2f3f6ba08
Narendra <nkadiri09@gmail.com>
2020-08-13 23:09:48 -0400

    Adding WebClient instead of RestTemplate

--- src/main/java/io/naren/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/io/naren/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +21,9 @@ public class MovieCatalogResource {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItems> getCatalog(@PathVariable String userId){
 
@@ -31,8 +35,16 @@ public class MovieCatalogResource {
 
      return ratings.stream().map( rating -> {
 
-        Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-        return new CatalogItems(movie.getName(), movie.getDesc(), rating.getRating());
+        //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+
+         Movie movie = webClientBuilder.build()
+                 .get()
+                 .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                 .retrieve()
+                 .bodyToMono(Movie.class)
+                 .block();
+
+         return new CatalogItems(movie.getName(), movie.getDesc(), rating.getRating());
 
     }).collect(Collectors.toList());
 
